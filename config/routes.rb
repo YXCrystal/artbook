269201d9ec0f5d786925151d8578c729e3c66e64
Crystal <yxcwuu@gmail.com>
2020-03-25 15:26:55 -0400

    Add posts form to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :profile, controller: "users", as: "users"
+  resources :profile, controller: "users", as: "users" do
+    resources :posts, only: [:new, :create, :show]
+  end
 
   authenticated :user do 
     root "users#index", as: "authenticated_root"
+
   end
 
   root "homepage#welcome"
