c9f402c79dbbe76a8e29149018122a4df5388669
Crystal <yxcwuu@gmail.com>
2020-03-25 15:25:50 -0400

    Add new and create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.create(post_params)
+        @post.user_id = current_user.id
+        if @post.save 
+            flash[:notice] = "Successfully posted!"
+            redirect_to '/profile'
+        else 
+            flash[:error] = "Something went wrong, please try again later."
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    private 
+    
+    def post_params
+        params.require(:post).permit(:body)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     def index
         @user = User.find_by(id: current_user)
+        @post = Post.new
+        @posts = Post.all
     end
 end
