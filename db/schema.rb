b41d6cbdd51fef0394d228f46a362875010635db
Crystal <yxcwuu@gmail.com>
2020-03-24 23:38:26 -0400

    Add birthday to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_033541) do
+ActiveRecord::Schema.define(version: 2020_03_25_033807) do
 
   create_table "users", force: :cascade do |t|
     t.string "location"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_033541) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
