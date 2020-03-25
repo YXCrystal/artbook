bd922141b82f0d987f7e22b260d5e003a34eb7f4
Crystal <yxcwuu@gmail.com>
2020-03-25 02:36:30 -0400

    Add seperate root route for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :profile, controller: "users", as: "users"
+
+  authenticated :user do 
+    root "users#index", as: "authenticated_root"
+  end
+
   root "homepage#welcome"
+
 end
