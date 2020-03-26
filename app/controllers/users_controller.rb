2ee1412b935579c498f7c600a2d1cffff819d7ac
Crystal <yxcwuu@gmail.com>
2020-03-26 17:03:26 -0400

    Add edit action and style edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:index, :show, :edit]
     def index
         @user = User.find_by(id: current_user)
         @post = Post.new
@@ -6,6 +7,15 @@ class UsersController < ApplicationController
     end
 
     def show 
+    end
+    
+    def edit
+    end
+
+    private
+
+    def find_user
         @user = User.find_by(id: current_user)
     end
+
 end
