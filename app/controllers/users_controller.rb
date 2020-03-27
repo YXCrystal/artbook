71bc286dc81559fe92186447cf5035deab67ccfa
Crystal <yxcwuu@gmail.com>
2020-03-27 00:29:46 -0400

    Allow users to change banner photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:index, :show, :edit]
+    before_action :find_user, only: [:index, :show, :edit, :update]
     def index
         @user = User.find_by(id: current_user)
         @post = Post.new
-        @posts = Post.all
+        @posts = Post.all.where(user_id: current_user.id)
+    end
+
+    def update
+        if @user.update(user_params)
+            flash.notice = "Profile has been updated"
+            redirect_to user_path(current_user.id)
+        else 
+            redirect_to 'edit'
+        end
     end
 
     def show 
@@ -15,7 +24,11 @@ class UsersController < ApplicationController
     private
 
     def find_user
-        @user = User.find_by(id: current_user)
+        @user = User.find_by(id: current_user.id)
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :birthday, :bio, :location, :avatar, :banner)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :banner, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :banner, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/clouds.jpg"
   validates_attachment_content_type :banner, content_type: /\Aimage\/.*\z/
 end
