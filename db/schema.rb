503878f6ef5e821cb4292885cf17fbac7d321a3d
Crystal <yxcwuu@gmail.com>
2020-03-27 15:26:07 -0400

    Add site attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_184121) do
+ActiveRecord::Schema.define(version: 2020_03_27_184058) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_184121) do
     t.string "banner_content_type"
     t.bigint "banner_file_size"
     t.datetime "banner_updated_at"
+    t.string "site"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
