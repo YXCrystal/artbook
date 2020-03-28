42c95636cfa3a3c808c9a59dd74aabe4c1ad588f
Crystal <yxcwuu@gmail.com>
2020-03-28 13:38:17 -0400

    Generate LikePost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_184058) do
+ActiveRecord::Schema.define(version: 2020_03_28_173002) do
+
+  create_table "like_posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
