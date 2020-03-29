b5cd1a1cbf19f42564aedc82298d365eff1c054d
Crystal <yxcwuu@gmail.com>
2020-03-29 17:10:11 -0400

    Gemerate Comments controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :like_posts
   has_many :likes, through: :like_posts, source: :post
+  has_many :comments
 
   has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default-profile.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
