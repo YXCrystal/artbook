56c15050ae72bb12e591bc6c0a6ef6b092c6efbf
Crystal <yxcwuu@gmail.com>
2020-03-29 17:58:00 -0400

    Add show action to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :like_posts
   has_many :liked_by, through: :like_posts, source: :user
+  has_many :comments
 
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
